3375cd5568d3267156405513fe893990012136e0
nizarIng <nizar.ilahi@gmail.com>
2025-04-10 13:18:48 +0100

    Calculate average ratings on Tour Model based on User Reviews (the 'update' and 'delete' phases).

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -63,15 +63,33 @@ reviewSchema.statics.calcAverageRatings = async function(tourId){
             }
         }
     ]);
-    console.log(stats);
-    await Tour.findByIdAndUpdate(tourId, {
-        ratingsQuantity: stats[0].nbRating,
-        ratingsAverage: stats[0].avgRating,
-    })
+    //console.log(stats);
+    if(stats.length > 0){
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nbRating,
+            ratingsAverage: stats[0].avgRating,
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5,
+        });
+    }
 }
 reviewSchema.post('save', function () {
     // "this" points to the current Review Model
     this.constructor.calcAverageRatings(this.tour);
+});
+
+//findByIdAndUpdate + findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.r = await this.findOne();
+    //console.log(this.r);
+    next();
+});
+reviewSchema.post(/^findOneAnd/, async function () {
+    // await this.findOne(); we cannot use this here since wo don't have access to the current query. the query has already executed.
+    await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
