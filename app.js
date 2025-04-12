876e2b59b21c6bc258cb772bfc3817832ce76dba
nizarIng <nizar.ilahi@gmail.com>
2025-04-12 12:55:20 +0100

    First steps with pug and starting giving ouw own styles.

--- app.js
+++ app.js
@@ -69,7 +69,10 @@ app.use((req,res,next)=>{
 
 // 2) ROUTES
 app.get('/', (req, res) => {
-    res.status(200).render('base');
+    res.status(200).render('base', {
+        tour: 'The Forest Hiker',
+        user: 'Nizar ILAHI'
+    });
 });
 
 app.use('/api/v1/tours', tourRouter);
