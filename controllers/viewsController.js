8649ab10dd510542733dd069e919b21347753fd2
nizarIng <nizar.ilahi@gmail.com>
2025-04-14 17:03:04 +0100

    Building the Tour overview page.

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,9 +1,14 @@
+const Tour = require("../models/tourModel");
+const catchAsync = require("../error-handlers/catchAsync");
 
-const getOverview = (req, res) => {
+const getOverview = catchAsync( async (req, res, next) => {
+    // 1) Get our tours data from the tour collection
+    const tours = await Tour.find();
     res.status(200).render('overview', {
         title: 'All Tours',
+        tours
     })
-};
+});
 const getTour = (req, res) => {
     res.status(200).render('tour', {
         title: 'The Forest Hiker Tour',
